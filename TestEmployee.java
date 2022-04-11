27d2648e85b068a911c8170325d2add9f1a82ebb
Sahiti <satyavolu.sahiti@capgemini.com>
2022-04-11 11:52:57 +0530

    Fourth Commit

---
+++ TestEmployee.java
@@ -0,0 +1,9 @@
+package com.cappack1.model;
+
+public class TestEmployee {
+
+	public static void main(String args[]) {
+		Employee e = new Employee(1,"Manvi");
+		System.out.println(e.toString());
+	}
+}
